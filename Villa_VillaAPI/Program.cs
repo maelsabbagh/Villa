c0e9e736134f3c3170f9eac978c848c479d6dec7
Mohamed Amr <m.aelsabbagh@gmail.com>
2025-10-22 16:11:09 +0300

    replace openAPI with swagger

--- Villa_VillaAPI/Program.cs
+++ Villa_VillaAPI/Program.cs
@@ -3,19 +3,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/openapi/v1.json", "VillaAPI");
-    });
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
