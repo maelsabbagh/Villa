c6b418a72bc310386009869b3c3d91f3038f5ef4
Mohamed Amr <m.aelsabbagh@gmail.com>
2025-10-27 13:24:57 +0300

    add villa utility project (class library)

---
+++ Villa_Utility/StaticDetails.cs
@@ -0,0 +1,13 @@
+﻿namespace Villa_Utility
+{
+    public static class StaticDetails
+    {
+        public enum ApiType
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }
+    }
+}
