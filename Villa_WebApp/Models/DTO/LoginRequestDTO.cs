43fa4dd07758a6a4c5340d61dcba3de8ab40d833
Mohamed Amr <m.aelsabbagh@gmail.com>
2025-11-05 17:29:41 +0200

    ADD Login/Register DTOs to web project

---
+++ Villa_WebApp/Models/DTO/LoginRequestDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Villa_Villa_WebApp.Models.DTO
+{
+    public class LoginRequestDTO
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
---
+++ Villa_WebApp/Models/DTO/RegistrationRequestDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace Villa_Villa_WebApp.Models.DTO
+
+namespace Villa_VillaAPI.Models.DTO
+{
+    public class RegistrationRequestDTO
+    {
+        
+        public string UserName { get; set; }
+        
+        public string Name { get; set; }
+     
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
